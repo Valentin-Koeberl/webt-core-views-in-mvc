d5cf218321badffd6cb9964f8043dd24c5f0af24
Valentin Köberl <valentin.koeberl@htl.rennweg.at>
2025-04-02 15:17:39 +0200

    Update to the design of MVC final

--- src/MVC_final.php
+++ src/MVC_final.php
@@ -8,20 +8,20 @@ function renderTemplate($templateFile, $templateFileBusinessEntity, $data): arra
 
     foreach ($data as $hotel) {
         $tmp = str_replace("{{name}}", $hotel['name'], $business_entity_template);
-        $hotelsHtml .= str_replace("{{description}}", $hotel['description'], $tmp);
+        $tmp2 = str_replace("{{description}}", $hotel['description'], $tmp);
+        $tmp3 = str_replace("{{image}}", $hotel['image'], $tmp2);
+        $hotelsHtml .= str_replace("{{score}}", $hotel['score'], $tmp3);
     }
 
     return str_replace("{{HOTEL}}", $hotelsHtml, $template);
 }
 
-// Liste zum hinzufügen anderer Hotels
 $hotels = [
-    ["name" => "Hotel Sacher, Vienna", "description" => "A legendary luxury hotel famous for the original Sachertorte."],
-    ["name" => "Hotel Imperial, Vienna", "description" => "A 5-star palace hotel offering a royal experience since 1873."],
-    ["name" => "Schloss Fuschl, Salzburg", "description" => "A stunning lakeside castle hotel with breathtaking Alpine views."],
-    ["name" => "Aqua Dome, Tyrol", "description" => "A world-famous spa resort surrounded by the Ötztal Alps."],
-    ["name" => "Biohotel Daberer, Carinthia", "description" => "A sustainable, eco-friendly retreat focused on organic wellness."]
+    ["name" => "Hotel Sacher, Vienna", "description" => "A legendary luxury hotel famous for the original Sachertorte.", "image" => "../src/18-BEL-03963-0048---Bellagio-Hero-Shot---Resize-v00PP.webp", "score" => "9.2"],
+    ["name" => "Hotel Imperial, Vienna", "description" => "A 5-star palace hotel offering a royal experience since 1873.", "image" => "../src/welcome-to-caesars-palace.jpg", "score" => "5.6"],
+    ["name" => "Schloss Fuschl, Salzburg", "description" => "A stunning lakeside castle hotel with breathtaking Alpine views.", "image" => "../src/283163011.jpg", "score" => "0.8"],
+    ["name" => "Aqua Dome, Tyrol", "description" => "A world-famous spa resort surrounded by the Ötztal Alps.", "image" => "../src/283163011.jpg", "score" => "9.8"],
+    ["name" => "Biohotel Daberer, Carinthia", "description" => "A sustainable, eco-friendly retreat focused on organic wellness.", "image" => "../src/18-BEL-03963-0048---Bellagio-Hero-Shot---Resize-v00PP.webp", "score" => "9.1"]
 ];
 
-//alles als html ausgeben
-echo renderTemplate("public/Prototype_us3.html", "public/business_entity.html", $hotels);
+echo renderTemplate("../public/Prototype_us3.html", "../public/business_entity.html", $hotels);
